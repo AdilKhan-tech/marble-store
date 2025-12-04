e2b12603a71e57c9f6145ebc19972daae0be7019
Shahab <shahabgul117@gmail.com>
2025-12-04 18:16:56 +0500

    working in icecream addons

--- server/routes/iceCreamRoutes.js
+++ server/routes/iceCreamRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const IceCreamBucketController = require('../controllers/IceCreamBucketController');
 const IceCreamPortionSizeController = require("../controllers/IceCreamPortionSizeController");
+const IceCreamAddOnsController = require("../controllers/IceCreamAddOnsController");
+
 const authenticateToken = require("../middlewares/authenticateToken");
 
 const upload = require("../middlewares/upload");
@@ -19,6 +21,13 @@ router.post("/portions", IceCreamPortionSizeController.createIceCreamPortionSize
 router.get("/portions", IceCreamPortionSizeController.getAllIceCreamPortionSize);
 router.put("/portion/:id", IceCreamPortionSizeController.updateIceCreamPortionSize);
 router.delete("/portion/:id", IceCreamPortionSizeController.deleteIceCreamPortionSizeById);
+
+
+router.post("/addons", IceCreamAddOnsController.createIceCreamAddOns);
+router.get("/addons", IceCreamAddOnsController.getAllIceCreamAddOns);
+router.put("/addons/:id", IceCreamAddOnsController.updateIceCreamAddOnsById);
+router.delete("/addons/:id", IceCreamAddOnsController.deleteIceCreamAddOnsById);
+
 
 
 module.exports = router;
