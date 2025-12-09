feb72b1aaca99a2fb39a8cd726ae55ad76efa3b2
Shahab <shahabgul117@gmail.com>
2025-12-09 18:06:06 +0500

    c

--- server/models/CustomCakeTypes.js
+++ server/models/CustomCakeTypes.js
@@ -10,38 +10,31 @@ const CustomCakeTypes = sequelize.define(
       autoIncrement: true,
       allowNull: false,
     },
-
     name_en: {
       type: DataTypes.STRING(255),
       allowNull: false,
     },
-
     name_ar: {
       type: DataTypes.STRING(255),
       allowNull: false,
     },
-
     slug: {
       type: DataTypes.STRING(255),
       allowNull: true,
     },
-
     status: {
       type: DataTypes.STRING(50),
       defaultValue: "true",
     },
-
     image_url: {
       type: DataTypes.STRING(500),
       allowNull: true,
     },
-
     created_at: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-
     updated_at: {
       type: DataTypes.DATE,
       allowNull: false,
