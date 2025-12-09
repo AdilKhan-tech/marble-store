8061d04ff41f28a26d2d79de47de75363812d071
Shahab <shahabgul117@gmail.com>
2025-12-09 17:41:44 +0500

    c

--- server/controllers/CustomCakeTypesController.js
+++ server/controllers/CustomCakeTypesController.js
@@ -11,25 +11,38 @@ class CustomCakeTypesController {
         status,
         image_url,
       });
-      return res.status(201).json({message: "Custom cake type created successfully",customcaketypes,});
+      return res
+        .status(201)
+        .json({
+          message: "Custom cake type created successfully",
+          customcaketypes,
+        });
     } catch (err) {
       console.error(err);
-      return res.status(500).json({message: "Failed to create custom cake type",error: err.message,});
+      return res
+        .status(500)
+        .json({
+          message: "Failed to create custom cake type",
+          error: err.message,
+        });
     }
   }
   static async getAllCustomCakeTypes(req, res) {
     try {
       const customcaketypes = await CustomCakeTypes.findAll();
       return res.status(200).json(customcaketypes);
-
-}
-    catch (err) {
+    } catch (err) {
       console.error(err);
-      return res.status(500).json({message: "Failed to retrieve custom cake types",error: err.message,});
+      return res
+        .status(500)
+        .json({
+          message: "Failed to retrieve custom cake types",
+          error: err.message,
+        });
     }
-}
+  }
   static async updateCustomCakeTypesById(req, res) {
-   try {
+    try {
       const { id } = req.params;
       const { name_en, name_ar, slug, status, image_url } = req.body;
       const customcaketypes = await CustomCakeTypes.findByPk(id);
@@ -43,26 +56,26 @@ class CustomCakeTypesController {
       customcaketypes.image_url = image_url;
 
       await customcaketypes.save();
-      return res.status(200).json({ message: "Custom cake type updated successfully", customcaketypes });
+      return res.status(200).json({message: "Custom cake type updated successfully",customcaketypes,});
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: error.message });
     }
-}
- static async deleteCustomCakeTypesById(req, res) {
-  const { id } = req.params;
-  try {
+  }
+  static async deleteCustomCakeTypesById(req, res) {
+    const { id } = req.params;
+    try {
       const customcaketypes = await CustomCakeTypes.findByPk(id);
       if (!customcaketypes) {
-          return res.status(404).json({ message: "Custom cake type not found" });
+        return res.status(404).json({ message: "Custom cake type not found" });
       }
       await customcaketypes.destroy();
       return res.status(200).json({ message: "Custom cake type deleted successfully" });
-  } catch (error) {
+    } catch (error) {
       console.error(error);
       return res.status(500).json({ message: error.message });
-}
-}
+    }
+  }
 }
 
 module.exports = CustomCakeTypesController;
