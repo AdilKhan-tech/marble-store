b6189b2ddd82ec9324c16ebd4fbd62f412c88bd2
AdilKhan-tech <adil.ce2007@gmail.com>
2025-12-17 17:34:28 +0500

    create a cookies page

---
+++ client/app/(dashboard)/dashboard/cookies/size/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import CookieBoxSize from '@/components/dashboard/cookies/CookieBoxSize'
+
+function page() {
+  return (
+    <div><CookieBoxSize /></div>
+  )
+}
+
+export default page
---
+++ client/components/dashboard/cookies/CookieBoxSize.js
@@ -0,0 +1,155 @@
+"use client"
+import React from 'react'
+import { useState , useEffect } from 'react'
+import axios from 'axios';
+import { getCookieBoxSizes } from '@/utils/apiRoutes';
+import {ToastContainer} from "react-toastify";
+import AddBoxSize from "@/components/dashboard/cookies/AddBoxSize";
+import useAxiosConfig from "@/hooks/useAxiosConfig"
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+function CookieBoxSize() {
+  const [boxSizes, setBoxSizes] = useState([]);
+  const {token} = useAxiosConfig();
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const [boxSizeData, setBoxSizeData] = useState(null);
+
+
+  const fetchBoxSizes = async () => {
+    try {
+    const response = await axios.get(getCookieBoxSizes);
+    setBoxSizes(response.data);
+    } catch (error) {
+      console.error("Error fetching box sizes", error);
+    }
+  }
+
+  useEffect(() => {
+    if(!token) return;
+    fetchBoxSizes();
+  }, [token]);
+
+  const showOffcanvasOnEditBoxSize = () => {
+    setBoxSizeData(null);
+    setShowOffcanvas(true);
+  }
+  const showOffcanvasOnAddBoxSize = (boxSize) => {
+    setBoxSizeData(boxSize);
+    setShowOffcanvas(true);
+  }
+
+  const closePopup = () => {
+    setShowOffcanvas(false);
+  };
+
+  const addBoxSizeToState = (newBoxSize) => {
+    setBoxSizes(prev => [newBoxSize, ...prev]);
+    setShowOffcanvas(false);
+    };
+  return (
+    <section className='' style={{marginTop:"100px"}}>
+      <div className=''>
+        <p className='pagetitle mb-0 fnt-color'>Cookie Box Size</p>
+        <div className='d-flex justify-content-between mt-4'>
+          <div className='d-flex'>
+            <i className='bi bi-search fs-20 py-1 px-2 text-secondary bg-light rounded-3 border rounded-end-0 border-end-0'></i>
+            <input
+              type='text'
+              className='form-control border-start-0 rounded-start-0'
+              placeholder='Search here...'
+              style={{height:"46px", width:"300px"}}
+            />
+          </div>
+          <div style={{marginInlineEnd:"20px"}}>
+            <div className='org-btn py-2 px-4 rounded-3'
+              onClick={showOffcanvasOnAddBoxSize}
+              role='button'>
+              <i className='bi bi-plus-circle ms-2'></i>
+              <span className='ms-1'>Create</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='px-0 pt-0 rounded-2 p-0 mt-3'>
+
+        <div className='datatable-wrapper'>
+          <div className='data-table p-2 rounded-4'>
+            <table className='table datatable datatable-table'>
+              <thead className=''>
+                <tr className=''>
+                  <th className='nowrap'>ID</th>
+                  <th className='nowrap'>Name</th>
+                  <th className='nowrap'>Cookies Type</th>
+                  <th className='nowrap'>Slug</th>
+                  <th className='fw-16 fnt-color'>Portion Size</th>
+                  <th className='fw-16 fnt-color'>Price</th>
+                  <th className='fw-16 fnt-color'>Symbol</th>
+                  <th className='fw-16 fnt-color'>Calories</th>
+                  <th className='fw-16 fnt-color'>Status</th>
+                  <th className='fw-16 fnt-color'>Action</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {boxSizes.map((boxSize, index) => (
+                  <tr key={`${boxSize.id}-${index}`}>
+
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.id}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.name_en}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.cookies_types_id}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.slug}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.portion_size}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.price}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.symbol}</span></td>
+                    <td className="fw-normal fnt-color"><span className='ms-1'>{boxSize.calories}</span></td>
+                    <td className="fs-16">
+                    <div className="form-check form-switch ms-4">
+                      <input className="form-check-input fs-4" type="checkbox" role="switch" name="status"
+                        />
+                    </div>
+                    </td>
+
+                    <td>
+                      <div className="d-flex gap-1">
+                        <button className="btn btn-sm btn-light p-2" onClick={() => showOffcanvasOnEditBoxSize(boxSize)}>
+                          <i className="bi bi-pencil text-primary"></i>
+                        </button>
+                        <button className="btn btn-sm btn-light p-2" onClick={() => showDeleteConfirmation(boxSize.id)}>
+                          <i className="bi bi-trash3 text-danger"></i>
+                        </button>
+                      </div>
+                    </td>
+
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <Offcanvas
+        show={showOffcanvas}
+        onHide={() => setShowOffcanvas(false)}
+        placement="end">
+        <Offcanvas.Header closeButton>
+        <Offcanvas.Title>
+            <div className='fs-24 fnt-color'>
+              {boxSizeData ? "Add Box Size" : "Update Box Size"}
+            </div>
+        </Offcanvas.Title>
+        </Offcanvas.Header>
+        <hr  className="mt-0"/>
+        <Offcanvas.Body>
+          <AddBoxSize
+            boxSizeData={boxSizeData}
+            closePopup={closePopup}
+            onAddBoxSize={addBoxSizeToState}
+        />
+        </Offcanvas.Body>
+    </Offcanvas>
+    <ToastContainer />
+      </div>
+    </section>
+  )
+}
+
+export default CookieBoxSize
